d1f19d22c62575bb8b7450cb47e3a37aeec3b7f6
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-04-04 21:24:28 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,15 +8,15 @@ class School
   end
   
   def add_student(student, grade)
-    
     if roster.include?(grade)
       roster[grade] << student
     else
       roster[grade] = []
     roster[grade] << student
     end
-    
   end
+  
+  
   
 end
 
